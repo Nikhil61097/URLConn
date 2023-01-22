4af94d121d1d1016621c73b2e309acccfbb60701
Nikhil61097 <122650806+Nikhil61097@users.noreply.github.com>
2023-01-22 01:07:54 -0400

    somethign

--- src/com/g7vfx/cs3103/JavaConn.java
+++ src/com/g7vfx/cs3103/JavaConn.java
@@ -1,6 +1,13 @@
 package com.g7vfx.cs3103;
 
-import java.net.URI;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.MalformedInputException;
 import java.util.Scanner;
 
 
@@ -8,8 +15,12 @@ import java.util.Scanner;
 
 public class JavaConn{
 	public static void main(String args[]) {
-		Link link = new Link(); 
-
+		String protocol = "https";
+		String domain = "cs3103.cs.unb.ca";
+		String docpath = "g7vfx/index.html";
+		String portno = "443";
+		String url;
+		
 		System.out.println("Choose one of the below :- "+"\n"+"1)Default URL\n"+"2)Custom URL\n");
 		Scanner sc = new Scanner(System.in);
 		int i = sc.nextInt();
@@ -17,48 +28,45 @@ public class JavaConn{
 		switch(i) {
 		case 1:
 			try {
-				URI myurl = new URI(link.url);
-				java.awt.Desktop.getDesktop().browse(myurl);
-			}
-			catch (Exception e) {
-				System.out.println(e);
-			}
-			break;
-		case 2:
+				url= protocol +"://"+ domain + ":" + portno + "/" + docpath;
+				System.out.println("Default link created : " + url );
+				
+				URL myurl = new URL("https://cs3103.cs.unb.ca:443/g7vfx/index.html");
 
-			System.out.println("Enter docpath\n");
-			String docpath_name= sc.next();
-			Link link1 = new Link(docpath_name);
-			try {
-				URI myurl1 = new URI(link1.url);
-				java.awt.Desktop.getDesktop().browse(myurl1);
+				try(
+						BufferedReader reader = new BufferedReader(new InputStreamReader(myurl.openStream()));
+						BufferedWriter writer = new BufferedWriter(new FileWriter("index.html"));
+						) {
+					String line;
+					while ((line = reader.readLine()) != null) {
+						writer.write(line);
+					}
+					System.out.println("Page downloaded.");
+
+				}
 			}
-			catch (Exception e) {
-				System.out.println(e);
+			catch(Exception e) {System.out.println(e);
 			}
-			break;
 		}
-
 	}
-
 }
 
 class Link {
 
 	String protocol = "https";
 	String domain = "cs3103.cs.unb.ca";
-	String docpath = "/g7vfx/index.html";
+	String docpath = "g7vfx/index.html";
 	String portno = "443";
 	String url;
 
 	Link(){
-		url= protocol +"://"+ domain  + docpath;
+		url= protocol +"://"+ domain + ":" + portno + "/" + docpath;
 		System.out.println("Default link created : " + url );
 	}	
 	Link(String docpath){
-		url= protocol + "://" + domain + docpath;
+		url= protocol +"://"+ domain + ":" + portno + "/"+ docpath;
 		System.out.println("Default link created : " + url );
 	}
 
-}		
+}	
 
